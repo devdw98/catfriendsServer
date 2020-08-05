af8e0a10d65b212ab9ba9172c60e3bc4f8a945ad
dw <devdw98@gmail.com>
2020-08-05 15:45:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# 냐한친구들 (가제) 서버
+
+### Spec
+- Gradle Project
+- Lang : Java 11
+- SpringBoot : 2.3.2
+- Packaging : Jar
+- DB : MySQL (변동 가능)
+- Dependencies (변동 가능)
+  * Spring Web
+  * Spring Session
+  * Spring Data JPA
+  * Lombok
+  * OAuth2 Client
+
+### 구현할 기능
+ - 사용자관리
+ - 결제 (구글 인앱 결제)
+ - 우편함 (FCM)
+ - 공지사항 (FCM)
+ - 광고 시청 시 돈 2배
+ 
---
+++ src/main/java/com/wtw/catfriendsServer/CatfriendsServerApplication.java
@@ -0,0 +1,13 @@
+package com.wtw.catfriendsServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatfriendsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatfriendsServerApplication.class, args);
+	}
+
+}
